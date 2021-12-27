0a22a7b3c3d6a2ff1b4b7444fca4a4d7526ee435
Jamieson Roberts <hello@jamiesonroberts.com>
2021-12-27 11:40:56 -0500

    Adding in Chakra UI baseline

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
 
-import '../styles/globals.css';
+import theme from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
