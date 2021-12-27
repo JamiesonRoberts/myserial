ed5e400178a806170286995638febb802d12194c
Jamieson Roberts <hello@jamiesonroberts.com>
2021-12-27 10:14:48 -0500

    refining TS before adding in auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
+import { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -66,4 +67,6 @@ export default function Home() {
       </footer>
     </div>
   );
-}
+};
+
+export default Home;
